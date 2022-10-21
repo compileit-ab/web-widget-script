c559936577a967b241545dbb7c954cf9b998a74a
T.J <tamim.se@hotmail.com>
2022-10-21 09:55:19 +0200

    test getting changes from jsdeliver

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ container.style.width ='300px'
 container.style.height='200px'
 const link = document.querySelector("div[data-business-id='David'] > a")
 const img = document.createElement('img')
-img.setAttribute('src', 'https://res.cloudinary.com/barncancerfonden/image/upload/v1666016270/BCF-widget/admin/xbkyh7nb6ripm9hjdyck.png')
+img.setAttribute('src', 'https://images.unsplash.com/photo-1598885159329-9377168ac375?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2FuY2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60')
 img.style.width='300px'
 img.style.height='200px'
 img.style.objectFit='contain'
