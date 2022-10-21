65aa7e66fdfbb23c3e790381ae2e446e361a4b09
T.J <tamim.se@hotmail.com>
2022-10-21 10:25:37 +0200

    change img

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ container.style.width ='300px'
 container.style.height='200px'
 const link = document.querySelector("div[data-business-id='David'] > a")
 const img = document.createElement('img')
-img.setAttribute('src', 'https://images.unsplash.com/photo-1598884143267-586d90a32141?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80')
+img.setAttribute('src', 'https://images.unsplash.com/photo-1495653797063-114787b77b23?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')
 img.style.width='300px'
 img.style.height='200px'
 img.style.objectFit='contain'
