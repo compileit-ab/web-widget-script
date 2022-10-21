f8d0606e82dc8a94b1af3c6e5d0d01aa5eca2264
T.J <tamim.se@hotmail.com>
2022-10-21 10:19:03 +0200

    test make changes to script

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ container.style.width ='300px'
 container.style.height='200px'
 const link = document.querySelector("div[data-business-id='David'] > a")
 const img = document.createElement('img')
-img.setAttribute('src', 'https://images.unsplash.com/photo-1598885159329-9377168ac375?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2FuY2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60')
+img.setAttribute('src', 'https://images.unsplash.com/photo-1598884143267-586d90a32141?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80')
 img.style.width='300px'
 img.style.height='200px'
 img.style.objectFit='contain'
