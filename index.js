12096939e6d24e21fa22001f039df1329a8d8ebe
T.J <tamim.se@hotmail.com>
2022-10-25 15:35:37 +0200

    update script

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const container = document.querySelector("div[data-bcf-business-id]")
 const companyName= container.getAttribute('data-bcf-business-id')
 const link = document.querySelector("div[data-bcf-business-id] > a")
 const img = document.createElement('img')
-img.setAttribute('src', `https://res.cloudinary.com/barncancerfonden/image/upload/BCF-widget/${companyName.toLowerCase()}/barncancerfonden.jpg`)
+// very important to include the date at the end to avoid getting old cached image
+const date= new Date()
+const imgSrc =`https://res.cloudinary.com/barncancerfonden/image/upload/BCF-widget/${companyName.toLowerCase()}/barncancerfonden.jpg?date=${date}`
+img.removeAttribute('src')
+img.setAttribute('src', imgSrc )
 img.style.width='100%'
 img.style.height='100%'
 link.appendChild(img)
